0ab934e3a0dc93d236913bcb90a4cf0cbc36b719
Petar Yankov <yankov.petar4@gmail.com>
2026-01-14 20:08:28 +0400

    add entity Account

---
+++ backend/src/main/java/com/yankov/backend/model/Account.java
@@ -0,0 +1,40 @@
+package com.yankov.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_number", unique = true)
+    private String accountNumber;
+
+    @Column(precision = 15, scale = 2)
+    private BigDecimal balance;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime created_At;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = LocalDateTime.now();
+    }
+}
