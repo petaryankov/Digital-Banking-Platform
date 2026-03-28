1d73721576e411bbdc9851a7e37298bf7cc3791f
Petar Yankov <yankov.petar4@gmail.com>
2026-03-28 11:04:15 +0200

    add find all

--- backend/src/main/java/com/yankov/backend/repository/UserRepository.java
+++ backend/src/main/java/com/yankov/backend/repository/UserRepository.java
@@ -1,11 +1,16 @@
 package com.yankov.backend.repository;
 
 import com.yankov.backend.model.User;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    @NonNull
+    List<User> findAll();
 }
